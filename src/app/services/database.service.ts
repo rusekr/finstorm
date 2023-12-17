ec8524e403bacb1d4868c2bab7337cff84fbcf5c
Sergey Krasnikov <eer@iie.su>
2023-12-17 23:37:02 +0300

    refs/heads/master WIP

---
+++ src/app/services/database.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+
+export interface Tag {
+  name: string;
+}
+
+export interface Wallet {
+  name: string;
+}
+
+export interface Transaction {
+  // TODO: добавить id: как ууид или инкремент
+  type: string, // тип - доход (income), расход (outcome)
+  date: Date; // дата транзакции
+  name: string; // имя 
+  sum: number; // сумма
+  tags: Tag[]; // тэги (категории)
+  wallet: Wallet; // связанный кошелёк
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+
+  private TAG_STORAGE: string = 'tags';
+  private WALLETS_STORAGE: string = 'wallets';
+  private TRANSACTIONS_STORAGE: string = 'transactions';
+ 
+  public tags: Tag[] = [];
+  public wallets: Wallet[] = [];
+  public transactions: Transaction[] = [];
+
+  constructor() { }
+
+  // получение по ключу  данных
+  private async getKeyData(key: string) {
+    let { value } = await Preferences.get({ key: key });
+    return (value ? JSON.parse(value) : []);
+  }
+
+  public async addNewTransaction(data: Transaction) {
+    this.transactions.unshift({
+      type: data.type,
+      date: data.date || new Date(),
+      name: data.name,
+      sum: data.sum,
+      tags: data.tags,
+      wallet: data.wallet 
+    });
+  }
+
+  public async loadData() {
+
+    this.tags = await this.getKeyData(this.TAG_STORAGE);
+    this.wallets = await this.getKeyData(this.WALLETS_STORAGE);
+    this.transactions = await this.getKeyData(this.TRANSACTIONS_STORAGE);
+  }
+
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,85 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OverlayEventDetail } from '@ionic/core/components';
+import { FormsModule } from '@angular/forms';
+import {
+  IonButton,
+  IonButtons,
+  IonFab,
+  IonFabButton,
+  IonHeader,
+  IonInput,
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonList, 
+  IonIcon, 
+  IonItem, 
+  IonLabel,
+  IonModal
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    CommonModule,
+    ExploreContainerComponent, 
+    IonButton,
+    IonButtons,
+    IonFab,
+    IonFabButton,
+    IonHeader,
+    IonInput,
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonIcon, 
+    IonInput,
+    IonList, 
+    IonItem, 
+    IonLabel,
+    IonModal,
+    FormsModule
+  ]
 })
 export class Tab2Page {
 
-  constructor() {}
+  // работа с модалом добавления, редактирования транзакции
+  @ViewChild(IonModal) modal: IonModal;
+  public message: string = '';
+  public name: string = '';
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
+
+  // работа с транзакциями
+  public transactions;
+  constructor(public databaseService: DatabaseService) {
+    this.transactions = databaseService.transactions;
+    //this.modal = 
+  }
+  // для оптимизации треканья транзакций т.к. их будут тонныы
+  trackTransactions(index: number, transactionObject: any) {
+    return index;// TODO: transactionObject.id;
+  }
+  showAddTransactionModal () {
+
+  }
+
+
 
 }
