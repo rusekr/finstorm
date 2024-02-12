47b5648770fd62e902f9ccc201d2aca83ddb8154
Sergey Krasnikov <eer@iie.su>
2024-02-12 23:18:13 +0300

    feat: общий доход, расход, бюджет

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -14,10 +14,29 @@ import { Transaction, TransactionType } from '../models/database.interface';
 })
 export class Tab1Page {
 
+  public incomeSum: number; // доход (общий или за отрезок времени)
+  public outcomeSum: number; // расход (общий или за отрезок времени)
+  public budgetSum: number; // бюджет (доход минус расход)
+
   public transactions: Transaction[];
   public transactionTypes = TransactionType;
 
   constructor(public databaseService: DatabaseService) {
     this.transactions = databaseService.getTransactions();
+
+    let sums = this.transactions.reduce((acc, cur) => {
+      if (cur.type === -1) {
+        acc.outcome += cur.sum;
+      } else {
+        acc.income += cur.sum;
+      }
+      return acc;
+    }, { income: 0, outcome: 0 });
+
+    this.incomeSum = sums.income;
+    this.outcomeSum = sums.outcome;
+
+    this.budgetSum = this.incomeSum - this.outcomeSum;
+
   }
 }
